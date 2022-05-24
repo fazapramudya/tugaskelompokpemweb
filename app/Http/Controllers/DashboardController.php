722cf50573d3f8e8485c84352a77f0eec6d66241
fazapramudya <pratprampray@gmail.com>
2022-05-24 23:25:23 +0700

    -

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,20 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
-        public function index2(){
-        return view('dashboard');
+    public function index()
+    {
+        return view('backend.dashboard');
     }
-        public function authenticate(Request $request){
-            $credentials = $request->validate([
-                'email' => 'required',
-                'password' => 'required'
-            ]);
-                return redirect('/dashboard');
-            
-        }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\RiwayatPembelianController;
-use App\Http\Controllers\barangController;
-use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\updatebarangcontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,26 +12,55 @@ use App\Http\Controllers\updatebarangcontroller;
 */
 
 Route::get('/', function () {
-    return view('login/login');
+    return redirect('/login');
 });
-Route::get('/register', [LoginController::class, 'index']);
-Route::post('/register', [LoginController::class, 'store']);
-Route::get('/dashboard', [DashboardController::class, 'index2']);
-Route::post('/dashboard', [DashboardController::class, 'authenticate']); 
-Route::get('/riwayatpembelian', [RiwayatPembelianController::class, 'index4']);
-Route::post('/riwayatpembelian', [RiwayatPembelianController::class, 'store']);
-Route::get('/barang', [barangController::class, 'index']);
-Route::post('/barang', [barangController::class, 'destroy']);
-Route::get('/pegawai', [PegawaiController::class, 'index']);
-Route::get('/edit-barang/{id}', [barangController::class, 'edit']);
-Route::post('/update-barang/{id}', [barangController::class, 'update']);
 
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile', 'HomeController@profile')->name('profile');
+Route::get('/profile/edit', 'HomeController@profileEdit')->name('profile.edit');
+Route::put('/profile/update', 'HomeController@profileUpdate')->name('profile.update');
+Route::get('/profile/changepassword', 'HomeController@changePasswordForm')->name('profile.change.password');
+Route::post('/profile/changepassword', 'HomeController@changePassword')->name('profile.changepassword');
 
-Route::delete('/pegawai/{id}', [PegawaiController::class, 'destroy']);
-Route::delete('/barang/{id}', [barangController::class, 'destroy']);
+Route::group(['middleware' => ['auth','role:Admin']], function () 
+{
+    Route::get('/roles-permissions', 'RolePermissionController@roles')->name('roles-permissions');
+    Route::get('/role-create', 'RolePermissionController@createRole')->name('role.create');
+    Route::post('/role-store', 'RolePermissionController@storeRole')->name('role.store');
+    Route::get('/role-edit/{id}', 'RolePermissionController@editRole')->name('role.edit');
+    Route::put('/role-update/{id}', 'RolePermissionController@updateRole')->name('role.update');
 
+    Route::get('/permission-create', 'RolePermissionController@createPermission')->name('permission.create');
+    Route::post('/permission-store', 'RolePermissionController@storePermission')->name('permission.store');
+    Route::get('/permission-edit/{id}', 'RolePermissionController@editPermission')->name('permission.edit');
+    Route::put('/permission-update/{id}', 'RolePermissionController@updatePermission')->name('permission.update');
 
+    Route::get('assign-subject-to-class/{id}', 'GradeController@assignSubject')->name('class.assign.subject');
+    Route::post('assign-subject-to-class/{id}', 'GradeController@storeAssignedSubject')->name('store.class.assign.subject');
 
+    Route::resource('assignrole', 'RoleAssign');
+    Route::resource('classes', 'GradeController');
+    Route::resource('subject', 'SubjectController');
+    Route::resource('teacher', 'TeacherController');
+    Route::resource('parents', 'ParentsController');
+    Route::resource('student', 'StudentController');
+    Route::get('attendance', 'AttendanceController@index')->name('attendance.index');
 
+});
 
+Route::group(['middleware' => ['auth','role:Teacher']], function () 
+{
+    Route::post('attendance', 'AttendanceController@store')->name('teacher.attendance.store');
+    Route::get('attendance-create/{classid}', 'AttendanceController@createByTeacher')->name('teacher.attendance.create');
+});
+
+Route::group(['middleware' => ['auth','role:Parent']], function () 
+{
+    Route::get('attendance/{attendance}', 'AttendanceController@show')->name('attendance.show');
+});
+
+Route::group(['middleware' => ['auth','role:Student']], function () {
+
+});
